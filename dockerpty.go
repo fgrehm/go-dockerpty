ca7d961593c42103f4324db0248811b8ce6166a9
Fabio Rehm <fgrehm@gmail.com>
2014-07-28 19:43:09 -0300

    Remove workaround for https://github.com/fsouza/go-dockerclient/issues/117
    Thanks @fsouza! <3

--- dockerpty.go
+++ dockerpty.go
@@ -13,7 +13,6 @@ func Start(client *docker.Client, container *docker.Container, hostConfig *docke
 		terminalFd    uintptr
 		oldState      *term.State
 		out           io.Writer     = os.Stdout
-		exitedChannel chan struct{} = make(chan struct{})
 	)
 
 	if file, ok := out.(*os.File); ok {
@@ -22,44 +21,25 @@ func Start(client *docker.Client, container *docker.Container, hostConfig *docke
 		return errors.New("Not a terminal!")
 	}
 
-	// This goroutine will listen to Docker events and will signal that is has
-	// stopped at the exitedChannel
-	go listenForContainerExit(client, container.ID, exitedChannel)
-
 	// Set up the pseudo terminal
 	oldState, err = term.SetRawTerminal(terminalFd)
 	if err != nil {
 		return
 	}
 
-	// Attach to the container on a separate goroutine
-	go attachToContainer(client, container.ID)
-
-	// And finally start it
+	// Start it
 	err = client.StartContainer(container.ID, hostConfig)
 	if err != nil {
 		return err
 	}
 
-	// Wait until the container has exited
-	<-exitedChannel
+	// Attach to the container
+	attachToContainer(client, container.ID)
 
 	// Clean up after the container has exited
-	term.RestoreTerminal(terminalFd, oldState)
+	defer term.RestoreTerminal(terminalFd, oldState)
 
 	return err
-}
-
-func listenForContainerExit(client *docker.Client, containerID string, exitedChannel chan struct{}) error {
-	listenerChannel := make(chan *docker.APIEvents)
-	client.AddEventListener(listenerChannel)
-
-	for {
-		event := <-listenerChannel
-		if event.ID == containerID && event.Status == "die" {
-			exitedChannel <- struct{}{}
-		}
-	}
 }
 
 func attachToContainer(client *docker.Client, containerID string) {
